f4d882a6368480e97742d822f7c03edfa4c3a977
Theodore Bailey <tawbailey@hotmail.com>
2023-08-15 08:32:57 +0100

    added comments

--- js.js
+++ js.js
@@ -1,11 +1,20 @@
 // add booeing to wrong answers
 // add confetti to correct answers with cheers
 
+//  begin by targeting elements
+// we have our question element, to populate with object items
 const questionElement = document.getElementById("question");
+// we have our button elements, to populate with answers
 const answerBtn = document.getElementById("answer-btn");
+// we have our next button to initiate function to repopulate with new answers
 const nextBtn = document.getElementById("next-btn");
+// we have our timer element to increment time
 const timerEle = document.getElementById("timer");
 
+// we will store our questions in an object
+// our questions and answers will create their own divisions
+// questions will be a key : value pair
+// answers will be an array of answers as the value with a key
 const questions = [
     {
         question: "Which is largest animal in the world?",
@@ -95,11 +104,15 @@ const questions = [
 ];
 
 // set quiz to initial index 0
+// we will need to on initiation set current question index to 0
 let currentQuestionIndex = 0;
 // set score to 0
+// we will need to initiate score as 0
 let score = 0;
 
 // create function to execute on starting quiz
+// startQuiz will set question index and score to 0
+// will populate next button, exectue showQuestions and set our timer
 function startQuiz() {
     // on start quiz initial values to 0, in case quiz has been completed
     currentQuestionIndex = 0;
@@ -158,6 +171,7 @@ function resetState(){
 // function selectAnswer with parameter of event.
 // select button is event.target property, isCorrect is select button dataset properties correct property to find if question is true, if isCorrect is truet then the selected button is given a class of correct, else incorrect
 function selectAnswer(e){
+    // select answer will target our event 
     const selectBtn = e.target;
     const isCorrect = selectBtn.dataset.correct === "true";
     if (isCorrect) {
@@ -168,16 +182,28 @@ function selectAnswer(e){
         selectBtn.classList.add("incorrect");
     }
     
-    // 
+    // for our correct answer, create array populate and idenitfy correct
+    // create an array from answerBtn children elements
+    // forEach element iterate through the button
+    // using the button parameter as an identifier
+    // if buttons data set property is correct, i.e, true
+    // add the correct class
     Array.from(answerBtn.children).forEach(button => {
         if (button.dataset.correct === "true"){
             button.classList.add("correct");
         }
+        // disable the buttons
         button.disabled = true;
     });
+
     nextBtn.style.display = "block";
 }
 
+// show score needs to:
+// resetState() to remove buttons
+// display score using score + number of questions for mutability
+// next button becomes play again
+// change display style of nextBtn
 function showScore() {
     resetState();
     questionElement.innerHTML = `You scored ${score} out of ${questions.length}.`;
@@ -185,6 +211,7 @@ function showScore() {
     nextBtn.style.display = "block";
 }
 
+// next button executes x times until equal to length of questions, then it will execute showScore
 function handleNextBtn() {
     currentQuestionIndex++;
     if(currentQuestionIndex < questions.length) {
@@ -194,6 +221,8 @@ function handleNextBtn() {
     }
 }
 
+// next button will execute a function
+// 
 nextBtn.addEventListener("click", ()=> {
     if (currentQuestionIndex < questions.length) {
         handleNextBtn();
@@ -202,10 +231,11 @@ nextBtn.addEventListener("click", ()=> {
     }
 });
 
-
+// my created timer
 function startTimer(duration, element) {
-
+    // set time to duration, the parameter countdown (not currently used)
     let time = duration;
+    // create minutes and seconds
     let minutes = 0;
     let seconds = 0;
 
@@ -217,6 +247,9 @@ function startTimer(duration, element) {
 
         element.innerHTML = minutes + ":" + seconds;
 
+        // when time is less than 0
+        // time = duration
+        // add 1 to minutes
         if (--time < 0) {
             time = duration;
             ++minutes;
@@ -224,4 +257,5 @@ function startTimer(duration, element) {
     }, 1000);
 }
 
+// execute the beggining of the quiz
 startQuiz();
